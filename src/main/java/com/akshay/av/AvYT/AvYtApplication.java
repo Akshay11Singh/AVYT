ed69c7c7b95170df12562005e1a147336006a6f9
Akshay11Singh <akshay1106singh@gmail.com>
2020-05-16 12:40:47 +0530

    initial commit

--- src/main/java/com/akshay/av/AvYT/AvYtApplication.java
+++ src/main/java/com/akshay/av/AvYT/AvYtApplication.java
@@ -1,11 +1,12 @@
 package com.akshay.av.AvYT;
 
+/*** Copyright@2020 Yeppie Technologies..... ***/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-/***Code Done for Yeppie Technologies...***/
+
 @SpringBootApplication
 @ComponentScan(value = "com.akshay.av.*")
 @EnableJpaRepositories(value = "com.akshay.av.repository")
@@ -15,25 +16,23 @@ public class AvYtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AvYtApplication.class, args);
 	}
-	
+
 }
 
-/*@Component
-@ConfigurationProperties
-@Validated
- class GlobalProperties {
-	 @Value(spring.jpa.properties.hibernate.jdbc.batch_size)
-    private int batchSize;
-
-	public int getThreadPool() {
-		return threadPool;
-	}
-
-	public void setThreadPool(int threadPool) {
-		this.threadPool = threadPool;
-	}
-
-    //getters and setters
-}
-
-*/
+/*
+ * @Component
+ * 
+ * @ConfigurationProperties
+ * 
+ * @Validated class GlobalProperties {
+ * 
+ * @Value(spring.jpa.properties.hibernate.jdbc.batch_size) private int
+ * batchSize;
+ * 
+ * public int getThreadPool() { return threadPool; }
+ * 
+ * public void setThreadPool(int threadPool) { this.threadPool = threadPool; }
+ * 
+ * //getters and setters }
+ * 
+ */
